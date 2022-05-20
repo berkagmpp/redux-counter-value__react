f98922a3eda32c355850fc3ba765b877db470c84
berkagmpp <berkagmpp@gmail.com>
2022-05-20 14:20:59 +1200

    apply useSelector() for showing Auth(login_input) and UserProfile in App.js / nav in Header.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
 
-import Header from './components/Header'
-import Auth from './components/Auth'
+import Header from './components/Header';
+import Auth from './components/Auth';
 import Counter from './components/Counter';
+import UserProfile from './components/UserProfile';
 
+function App(props) {
+    const isAuth =  useSelector(state => state.authentication.isAuthenticated);
 
-function App() {
     return (
         <Fragment>
             <Header />
-            <Auth />
+            {!isAuth && <Auth />}
+            {isAuth && <UserProfile />}
             <Counter />
         </Fragment>
     );
